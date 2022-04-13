02f57134b7d4b0da9cfea5c4df10efa5edf58bdc
VLSLV333 <v.sherstyk@gmail.com>
2022-04-13 13:03:49 +0000

    Generators sound useless, mb need more knowledge. yield!

--- index.js
+++ index.js
@@ -44,28 +44,62 @@
 // }
 
 
+// for (let k of iter(78)){          // 
+//     console.log(k)
+// }
 
-// const iterator = {          // повторили логіку генератора
-//     [Symbol.iterator](n = 10){
+
+
+
+
+// function* strGenerator() {
+//     yield "H"
+//     yield "e"
+//     yield "l"
+//     yield "l"
+//     yield "o"
+// }
+// const str = strGenerator();
+
+// for (let i = 0; i < str.length; i++){
+//    yield str
+// }
+
+// for (let key of str) console.log(str.next());
+
+// function* numGen(n = 33) {
+//     for (let i = 0; i < n; i++){
+//        yield console.log(i)
+//     }
+// }
+// const v = numGen(8)
+
+
+// const  iterator = {
+//     [Symbol.iterator](n = 10){          // [Symbol.iterator] = дозволяє використовувати for of метод для нашої об'єкту. Лише об'єкти з функцією [Symbol.iterator] можуть бути проітеровані в for of
 //         let i = 0
+
 //         return {
 //             next (){
 //                 if (i < n){
-//                     return {value: ++i, done: "false"}
-//                 } 
-//                     return {value: undefined, done: "true"}
+//                     return {
+//                         value: ++i, done: false
+//                     }
+//                 } return {
+//                     value: undefined, done: true
+//                 }
 //             }
 //         }
 //     }
 // }
 
-function* iter( n = 10){
+
+function* iter (n = 22){
     for (let i = 0; i < n; i++){
-        yield i;
+        yield i
     }
 }
 
-
-for (let k of iter(78)){          // 
-    console.log(k)
+for (let key of iter(8)){
+    console.log(key)
 }
